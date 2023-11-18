cb4c5119990453694f8c12d8f0d80917d1aa216a
pankv1 <cb.pankiv@gmail.com>
2023-11-19 00:17:21 +0200

    corrections

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
           Hello, I'm <span className="font-bold">Illia</span>. I'm a <span className="font-bold">wordpress developer</span>. I thrive on turning ideas into functional and visually appealing websites. Let's bring your digital vision to life!
         </h1>
         <div className=" flex flex-col sm:flex-row gap-2 justify-center">
-          <a className="flex justify-center items-center gap-3 bg-black px-12 py-2 text-white rounded-3xl hover:bg-gray-800 active:bg-gray-950 font-semibold" href="">Contact me<FiMail/></a>
+          <a className="flex justify-center items-center gap-3 bg-black px-12 py-2 text-white rounded-3xl hover:bg-gray-800 active:bg-gray-950 font-semibold" href="#contact">Contact me<FiMail/></a>
           <a className="flex justify-center items-center gap-3 bg-white px-12 py-2 text-black rounded-3xl hover:bg-gray-100 active:bg-gray-50 font-semibold" href={cv} download={true}>Download CV<FiDownload/></a>
           <a href=""><img src="" alt="" /></a>
         </div>
